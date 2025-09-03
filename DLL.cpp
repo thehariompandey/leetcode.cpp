1e05ac74c4fdaaeb2d8b79b97e670288d52e69ce
Hariom Pandey <hariompandey0349@gmail.com>
2025-09-03 12:20:44 +0530

    doubly linkedlist

---
+++ DLL.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std ;
+
+class Node{
+    public:
+    int data ;
+    Node* next ;
+    Node* prev ;
+
+    // constructor
+    Node(int data){
+        this->data = data;
+        this->next = nullptr;
+        this->prev = nullptr;
+    }
+
+    // constructor with all arguments
+    Node(int data, Node* next, Node* prev){
+        this->data = data;
+        this->next = next;
+        this->prev = prev;
+    }
+    
+};
+
+Node* convertArr2DLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+
+    for(int i = 1 ; i<arr.size() ; i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp ;
+        prev = temp;
+    }
+    return head;
+}
+
+void print(Node* &head){
+    while(head != nullptr){
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+int main(){
+    vector<int> arr= {2,4,5,6};
+    Node* head = convertArr2DLL(arr);
+    print(head);
+
+}
