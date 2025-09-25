6aa602ba2a1b3b1ba4c8702f5560e51ce9c320cc
Hariom Pandey <hariompandey0349@gmail.com>
2025-09-25 12:13:44 +0530

    stack implementation by array and linkedlist both

--- stimp.cpp
+++ stimp.cpp
@@ -2,65 +2,122 @@
 #include<stack>
 using namespace std;
 
+// class Stack{
+//         // properties
+//     public:
+//     int *arr ;
+//     int top ;
+//     int size;
+//     // construtor 
+
+//     Stack(int size){
+//         this -> size = size;
+//         arr = new int[size];
+//         top = -1;
+//     }
+
+
+// void push(int element){
+//     if(size - top > 1){
+//         top++ ;
+//         arr[top] = element ;
+//     }
+//     else{
+//         cout << "stack overflow" << endl;
+//     }
+// }
+
+// void pop(){
+//     if(top >= 0){
+//         top-- ;
+//     }
+//     else{
+//         cout << "stack underflow" << endl;
+//     }
+// }
+
+// int peek(){
+//     if(top >= 0 && top<size){
+//         return arr[top];
+//     }
+//     else{
+//         cout << "stack is empty" << endl;
+//         return -1;
+//     }
+// }
+
+// bool isEmpty(){
+//     if(top == -1){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// };
+
+
+// Now implementing stack using linkedlist 
+
+struct Node
+{
+    int data;
+    Node* next ;
+
+    Node(int data){
+        this -> data = data ;
+        this -> next = nullptr;
+    }
+};
+
+
 class Stack{
-        // properties
+    Node* top ; // head node as stack top 
     public:
-    int *arr ;
-    int top ;
-    int size;
-    // construtor 
-
-    Stack(int size){
-        this -> size = size;
-        arr = new int[size];
-        top = -1;
+    Stack(){
+        top = nullptr;
     }
 
+    void push(int x){
+        Node* newNode = new Node(x);
+        newNode -> next = top;
+        top = newNode;
+        cout << x << "pushed into stack" << endl;
+    }
 
-void push(int element){
-    if(size - top > 1){
-        top++ ;
-        arr[top] = element ;
-    }
-    else{
-        cout << "stack overflow" << endl;
-    }
-}
+    void pop(){
+        if(isEmpty()){
+            cout << "stack underflow"  << endl;
+            return ;
+        }
+        Node* temp = top;
+        cout << top->data << "poped from stack" << endl;
+        top = top->next ;
+        delete temp;
+    };
 
-void pop(){
-    if(top >= 0){
-        top-- ;
-    }
-    else{
-        cout << "stack underflow" << endl;
-    }
-}
 
-int peek(){
-    if(top >= 0 && top<size){
-        return arr[top];
+    int peek(){
+        if(isEmpty()){
+            cout << "stack is empty" << endl;
+            return -1;
+        }
+        return top->data ;
     }
-    else{
-        cout << "stack is empty" << endl;
-        return -1;
-    }
-}
 
-bool isEmpty(){
-    if(top == -1){
-        return true;
+    bool isEmpty(){
+        return top == nullptr;
     }
-    else{
-        return false;
-    }
-}
-
 };
 
 
 
+
+
+
 int main(){
-    Stack st(5);
+    Stack st;
 
     st.push(22);
     st.push(23);
