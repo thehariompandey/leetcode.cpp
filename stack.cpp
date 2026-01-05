9f9e40dda645496f242f3a3cc50ccc5d78a16e09
Hariom Pandey <hariompandey0349@gmail.com>
2026-01-05 11:04:49 +0530

    stack implementation through array

--- stack.cpp
+++ stack.cpp
@@ -1,64 +1,68 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
+using namespace std ;
 
-class Stack {
-public:
-    int st[10];
-    int topIndex = -1;
 
-    // Push element
-    bool push(int x) {
-        if (topIndex >= 9) {   // max index = 9
-            cout << "Stack Overflow!" << endl;
-            return false;
-        }
-        topIndex++;
-        st[topIndex] = x;
+class Stack{
+    // properties 
+    public:
+    int size ;
+    int *arr ;
+    int top ;
+
+
+    // construtor
+    Stack(int size){ 
+    this -> size = size ;
+    arr = new int[size];
+    top = -1;
+    }
+
+
+void push(int element){
+    if(top < size -1){
+        top++ ;
+        arr[top] = element;
+    }else{
+        cout << "stack overflow" << endl;
+    }
+}
+
+void pop(){
+    if(top >= 0){
+        top-- ;
+    }else{
+        cout << "stack underflow" << endl;
+    }
+}
+
+int peek(){
+    if(top >= 0){
+        return arr[top];
+    }else{
+        cout << "stack is empty" << endl;
+        return -1;
+    }
+}
+
+bool isempty(){
+    if(top == -1){
         return true;
+    }else{
+        return false;
     }
-
-    // Return top element
-    int top() {
-        if (topIndex == -1) {
-            cout << "Stack is Empty!" << endl;
-            return -1;
-        }
-        return st[topIndex];
-    }
-
-    // Pop element
-    int pop() {
-        if (topIndex == -1) {
-            cout << "Stack Underflow!" << endl;
-            return -1;
-        }
-        int val = st[topIndex];
-        topIndex--;
-        return val;
-    }
-
-    // Return size
-    int size() {
-        return topIndex + 1;
-    }
-
-    // Check empty
-    bool empty() {
-        return topIndex == -1;
-    }
+}
 };
 
-int main() {
-    Stack s;
-    s.push(10);
-    s.push(20);
-    cout << "Top: " << s.top() << endl;
-    cout << "Popped: " << s.pop() << endl;
-    cout << "Size: " << s.size() << endl;
-    cout << "Is Empty: " << (s.empty() ? "Yes" : "No") << endl;
-    cout << "Top: " << s.top() << endl;
-    s.push(5);
-    cout << "Size :" << s.size() << endl; 
-    cout << "poped : " << s.pop() << endl;
-    cout << s.top() << endl;
+int main(){
+    Stack s(3);
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    cout << s.peek() << endl;
+
+    s.pop();
+    cout << s.peek() << endl;
 }
